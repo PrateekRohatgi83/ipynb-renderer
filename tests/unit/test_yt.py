548b324af52920a285cd97a1cb6cd21a243fdc31
PrateekRohatgi83 <rohatgiprateek2014@gmail.com>
2023-07-06 23:01:46 +0530

    add unit test

--- tests/unit/test_yt.py
+++ tests/unit/test_yt.py
@@ -21,6 +21,7 @@ URL_id_bad_data = [
 def test_get_time_info(URL, response):
     assert get_time_info(URL) == response
 
+
 @pytest.mark.parametrize("URL", URL_id_bad_data)
 def test_get_time_info_failed(URL):
     with pytest.raises(InvalidURLException):
